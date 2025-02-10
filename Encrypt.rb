2a7ca41bf68e4153b1d3e6973499a4b40c930582
EastTimorGhostSec <defanjesus@gmail.com>
2025-02-10 23:45:06 +0900

    Ip-ScannerV1.0.rb

--- Encrypt.rb
+++ Encrypt.rb
@@ -8,17 +8,17 @@ def encrypt_code(plain_text, key, iv)
   cipher.key = key
   cipher.iv = iv
 
-  compressed_code = Zlib::Deflate.deflate(plain_text)  # Kompres kode
+  compressed_code = Zlib::Deflate.deflate(plain_text)  
   encrypted = cipher.update(compressed_code) + cipher.final
 
-  Base64.strict_encode64(encrypted)  # Encode ke Base64 agar bisa disimpan
+  Base64.strict_encode64(encrypted)  
 end
 
-key = Digest::SHA256.digest("Catania")  # Buat kunci enkripsi
-iv = "a1b2c3d4e5f6g7h8"  # IV harus tetap sama untuk dekripsi
+key = Digest::SHA256.digest("Catania")  
+iv = "a1b2c3d4e5f6g7h8" 
 
-# 🔥 Baca kode asli dari `Port-ScannerV1.0.rb`
-original_code = File.read("Ip-ScannerV1.0.rb")  
+
+original_code = File.read("Ip-Ori.rb")  
 encrypted_code = encrypt_code(original_code, key, iv)
 
 puts "Kode terenkripsi:"
